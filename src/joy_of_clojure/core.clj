42f922f8e7babe7348c0038878f95c135e26f0c5
Ashish Negi <ashish@quintype.com>
2016-06-24 08:46:27 +0530

    Writing some tips as i go though the book.

--- src/joy_of_clojure/core.clj
+++ src/joy_of_clojure/core.clj
@@ -1,1 +1,27 @@
 (ns joy-of-clojure.core)
+
+;; Tips here
+
+;; 1. clojure.java.javadoc/javadoc gets you the docs for java apis.
+;; 2. Exceptions :
+;;    If macros caused a problem we would see : Compiler.java a lot
+;;    in stack traces.
+;; 3. (rest [])
+;;    ;;=> ()
+;;    (next [])
+;;    ;;=> nil
+;;    Use rest to get the rest of the values.
+;;    seq is good for checking the emptyness of nil of a sequence.
+;;    (seq [])
+;;    ;;=> nil
+;;    (seq nil)
+;;    ;=> nil
+;; 4.
+
+
+
+
+
+;; To do
+;; 1. What is symbol keys ?
+;; 2.
