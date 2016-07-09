faf65cda630ef09ecd03b6ff8de42be5a216d001
Ashish Negi <ashish@quintype.com>
2016-07-09 18:17:45 +0530

    Using deftype to create a new type Tree

---
+++ src/joy_of_clojure/chap9_combinig_data_and_code.clj
@@ -0,0 +1,47 @@
+(ns joy-of-clojure.chap9-combinig-data-and-code)
+
+(defprotocol FIXO
+  ;; First In X Out
+  (f-push [t v])
+  (f-pop [t])
+  (f-peek [t]))
+
+(deftype Tree [val l r]
+  FIXO
+  (f-push [_ v]
+    (if (< v val)
+      (Tree. val (f-push l v) r)
+      (Tree. val l (f-push r v))))
+  (f-pop [_]
+    (if l
+      (Tree. val (f-pop l) r)
+      r))
+  (f-peek [_]
+    (if l
+      (f-peek l)
+      val))
+
+  clojure.lang.Seqable
+  (seq [_]
+    (concat (seq l) [val] (seq r)))
+
+  Object
+  (toString [t]
+    (str (seq t))))
+
+(extend-type
+    nil FIXO
+    (f-push [_ v]
+      (Tree. v nil nil)))
+
+(def tree (Tree. 10 nil nil))
+;; => #'joy-of-clojure.chap9-combinig-data-and-code/tree
+
+(seq tree)
+;; => (10)
+
+(defn- f-into [t coll]
+  (reduce f-push t coll))
+
+(seq (f-into tree (range 10)))
+;; => (0 1 2 3 4 5 6 7 8 9 10)
